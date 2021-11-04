2147be6f79b4e485a522f54ed5d818a741c8f261
Abdullah ibn Nadjo <abdullahibnnadjo@gmail.com>
2021-11-04 16:15:06 +0100

    Ponyfill for requestAnimationFrame and perf.now

--- src/runtime/compat/index.ts
+++ src/runtime/compat/index.ts
@@ -1,17 +1,40 @@
+// pure ponyfills (do not pollute global namespace)
 import CompatMap from 'core-js-pure/stable/map';
 import CompatPromise from 'core-js-pure/stable/promise';
 import CompatSet from 'core-js-pure/stable/set';
 import CompatSymbol from 'core-js-pure/stable/symbol';
 
-// polyfills for runtime
+import _requestAnimationFrame from 'raf'; // environment.ts
+import performanceNow from 'performance-now';
+
+// global polyfills for runtime
 import 'core-js/modules/es.object.keys'; //dev.ts
 import 'core-js/modules/es.string.starts-with';
 import 'core-js/modules/es.array.fill';
 import 'core-js/modules/es.array.from'; 
 import 'core-js/modules/es.array.sort';
-
 import 'core-js/modules/es.typed-array.int32-array.js'; //dom.ts
 
-// Be sure to avoid for(x of y) syntax
+// Note: Be sure to avoid for(x of y) syntax
 
-export { CompatMap, CompatPromise, CompatSet, CompatSymbol };
+const PerfCompat = {
+    // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+    now: performanceNow
+}; 
+
+const Raf = {
+    requestAnimationFrame: (cb) => {    
+        if (typeof window !== 'object') {
+            // noop, we’re server side
+            return;
+        }
+
+        //@ts-expect-error window is checked
+        if (typeof window.requestAnimiationFrame === 'function') {
+            return window.requestAnimationFrame(cb);
+        }
+        return _requestAnimationFrame(cb);
+    }
+};
+
+export { CompatMap, CompatPromise, CompatSet, CompatSymbol, PerfCompat, Raf };
--- src/runtime/internal/environment.ts
+++ src/runtime/internal/environment.ts
@@ -1,12 +1,13 @@
 import { noop } from './utils';
+import { PerfCompat, Raf } from 'svelte/compat';
 
 export const is_client = typeof window !== 'undefined';
 
 export let now: () => number = is_client
-	? () => window.performance.now()
+	? () => PerfCompat.now()
 	: () => Date.now();
 
-export let raf = is_client ? cb => requestAnimationFrame(cb) : noop;
+export let raf = is_client ? cb => Raf.requestAnimationFrame(cb) : noop;
 
 // used internally for testing
 export function set_now(fn) {
